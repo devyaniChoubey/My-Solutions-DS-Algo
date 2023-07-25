4b5f93c9ee2342661ed0b2cbabf023b162616e26
devyani <devyani@devyani.local>
2023-07-25 12:48:49 +0530

    binary tree solutions

---
+++ Trees/Traversals/Morris Traversal Inorder.cpp
@@ -0,0 +1,32 @@
+threaded binary tree concept
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>inorder;
+        if(root == NULL) return inorder;
+        TreeNode* cur = root;
+        while(cur != NULL){
+            if(cur->left == NULL){
+                inorder.push_back(cur->val);
+                cur = cur->right;
+            }else{
+                TreeNode* prev = cur->left;
+                while(prev->right && prev->right != cur){
+                    prev = prev->right;
+                }
+
+                if(prev->right == NULL){
+                    prev->right = cur;
+                    cur = cur->left;
+                }else if(prev->right == cur){
+                    prev->right = NULL;
+                    inorder.push_back(cur->val);
+                    cur = cur->right;
+                }
+            }
+        }
+
+        return inorder;
+    }
+};
---
+++ Trees/Traversals/Morris Traversal preorder.cpp
@@ -0,0 +1,41 @@
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>preorder;
+        TreeNode* cur = root;
+        while(cur != NULL){
+            if(cur->left == NULL){
+                preorder.push_back(cur->val);
+                cur = cur->right;
+            }else{
+                TreeNode* prev = cur->left;
+                while(prev->right != NULL && prev->right != cur){
+                    prev = prev->right;
+                }
+
+                if(prev->right == NULL){
+                    prev->right = cur;
+                    preorder.push_back(cur->val);
+                    cur = cur->left;
+                }else if(prev->right == cur){
+                    prev->right == NULL;
+                    cur = cur->right;
+                }
+            }
+        }
+
+        return preorder;
+    }
+};
