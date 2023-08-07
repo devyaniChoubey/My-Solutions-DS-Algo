51fa79f7c02e264141fa219d981e21ce26cedd90
devyani <devyani@devyani.local>
2023-08-07 16:55:54 +0530

    DP solutions

--- DP/MCM/Burst balloons.cpp
+++ DP/MCM/Burst balloons.cpp
@@ -1,47 +1,54 @@
+https://leetcode.com/problems/burst-balloons/description/
+
+
+start recursion from the last balloon left
+//Recursive
 class Solution {
 public:
-    int solve(int i , int j, vector<int>&nums){
+    int solve(int i, int j, vector<int>& nums){
         if(i > j) return 0;
-          
+
         int maxi = -1e9;
         for(int k=i;k <= j;k++){
-            int val = solve(i, k-1,nums) + solve(k+1, j,nums) + nums[i-1]*nums[k]*nums[j+1];
-            maxi = max(maxi, val);
+            int totalCost = solve(i, k-1, nums) + nums[i-1]*nums[k]*nums[j+1] + solve(k+1, j, nums);
+            maxi = max(maxi, totalCost);
         }
 
         return maxi;
     }
     int maxCoins(vector<int>& nums) {
-        nums.push_back(1);
         nums.insert(nums.begin(), 1);
+        nums.push_back(1);
         int n = nums.size();
+
         return solve(1, n-2, nums);
     }
 };
 
-zz
+
 
 //Memoisation
-
 class Solution {
 public:
-    int solve(int i , int j, vector<int>&nums,vector<vector<int>>&dp){
+    int solve(int i, int j, vector<int>& nums,vector<vector<int>>&dp){
         if(i > j) return 0;
-          
-        int maxi = -1e9;
+
         if(dp[i][j] != -1) return dp[i][j];
+        int maxi = -1e9;
         for(int k=i;k <= j;k++){
-            int val = solve(i, k-1,nums,dp) + solve(k+1, j,nums,dp) + nums[i-1]*nums[k]*nums[j+1];
-            maxi = max(maxi, val);
+            int totalCost = solve(i, k-1, nums,dp) + nums[i-1]*nums[k]*nums[j+1] + solve(k+1, j, nums,dp);
+            maxi = max(maxi, totalCost);
         }
 
         return dp[i][j] = maxi;
     }
     int maxCoins(vector<int>& nums) {
-        nums.push_back(1);
         nums.insert(nums.begin(), 1);
+        nums.push_back(1);
         int n = nums.size();
-        vector<vector<int>>dp(n, vector<int>(n, 0));
+
+        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+
         return solve(1, n-2, nums,dp);
     }
 };
