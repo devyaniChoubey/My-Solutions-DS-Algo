bc48665c700dd8d31e8792aa962f3732beb14856
devyani <devyani@devyani.local>
2023-09-26 11:47:33 +0530

    graph solutions

---
+++ Trees/Subtree of Another Tree.cpp
@@ -0,0 +1,32 @@
+https://leetcode.com/problems/subtree-of-another-tree/description/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool helper(TreeNode* root, TreeNode* subRoot){
+        if(root == NULL || subRoot == NULL) return (root == subRoot);
+
+        if(root->val != subRoot->val) return false;
+
+        return (helper(root->left, subRoot->left) && helper(root->right, subRoot->right));
+    }
+    
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(root == NULL) return false;
+
+        if(helper(root, subRoot)) return true;
+        
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+    }
+};
