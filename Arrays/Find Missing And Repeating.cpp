d34f92e5793578606254374d642493f3578ed5f1
devyani <devyani@devyani.local>
2023-10-24 07:59:18 +0530

    array changes

--- Arrays/Find Missing And Repeating.cpp
+++ Arrays/Find Missing And Repeating.cpp
@@ -1,5 +1,6 @@
 https://practice.geeksforgeeks.org/problems/find-missing-and-repeating2512/1
 
+https://www.interviewbit.com/problems/repeat-and-missing-number-array/
 
 class Solution{
 public:
@@ -25,30 +26,26 @@ public:
 
 
 
+vector<int> Solution::repeatedNumber(const vector<int> &arr) {
+    long long n = arr.size();
+    long long SN = ((n)*(n+1))/2;
 
-class Solution{
-public:
-    vector<int> findTwoElement(vector<int> arr, int N) {
-        long long int n = arr.size();
-        
-        long long int SN = (n * (n+1) ) /2 , S2N = (n * (n +1) *(2*n +1) ) / 6;
-        
-         long long int S = 0, S2 = 0;
-        
-        for(int i=0;i < n;i++){
-            S += ( long long int) arr[i];
-            S2 += ( long long int) arr[i]*( long long int)arr[i];
-        }
-        
-        long long int val1 = (S-SN);
-        long long int val2 = (S2-S2N);
-        
-        long long int repeatingEl =  ((val2)/(val1)) + (val1);
-        
-        repeatingEl = repeatingEl/2;
-        
-        long long int missing = repeatingEl - val1;
-        return {(int)repeatingEl, (int)missing};
-        
-    }
-};
+	long long S2N = ((n)*(n+1)*(2*n + 1))/6;
+
+	long long S = 0, S2 = 0;
+
+	for(int i=0;i < n;i++){
+		S += (long long) arr[i];
+		S2 += (long long) arr[i] * (long long) arr[i];
+	}
+
+	long long val1 = S - SN;
+	long long val2 = S2 - S2N;
+
+    val2 = val2/val1;
+
+	long long r = (val2 + val1)/2;
+
+
+	return {(int)r , (int)(r - val1)};
+}
---
+++ Arrays/Largest Number.cpp
@@ -0,0 +1,54 @@
+https://leetcode.com/problems/largest-number/description/
+
+string Solution::largestNumber(const vector<int> &nums) {
+    vector<string>arr;
+
+    for(auto num:nums){
+        arr.push_back(to_string(num));
+    }
+
+    sort(arr.begin() , arr.end()  , [](string &s1, string &s2) {
+        return s1 + s2 > s2 + s1;
+    });
+
+    string res;
+
+    for(string &s:arr){
+        res += s;
+    }
+
+    while(res.size() > 1 && res[0] == '0'){
+        res.erase(res.begin());
+    }
+
+    return res;
+}
+
+
+Find Permutation
+
+
+class Solution {
+public:
+    vector<int> findPermutation(string s) {
+        int n = s.length();
+
+        vector<int>res(n+1);
+
+        for(int i=0;i <= n;i++) res[i] = i+1;
+
+        int i = 1, j;
+
+        while(i <= n+1){
+            j = i;
+
+            while(i <= n && s[i-1] == 'D') i++;
+
+            reverse(res.begin() + j - 1, res.begin() + i);
+
+            i++;
+        }
+
+        return res;
+    }
+};
